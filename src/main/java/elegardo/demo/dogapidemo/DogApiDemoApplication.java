757b1846cf7ee0d1946f77e0a2e8f8c1191bcc8a
Elegardo Valdés <elegardo.valdes@equifax.com>
2018-02-26 23:40:35 -0300

    configura proyecto con spring Initializr

---
+++ src/main/java/elegardo/demo/dogapidemo/DogApiDemoApplication.java
@@ -0,0 +1,12 @@
+package elegardo.demo.dogapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogApiDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/elegardo/demo/dogapidemo/DogApiDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package elegardo.demo.dogapidemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DogApiDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

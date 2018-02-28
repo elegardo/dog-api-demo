5e059af997fc7bee36897fced52f1ab4a5e9c790
Elegardo Valdés <elegardo.valdes@equifax.com>
2018-02-28 01:27:02 -0300

    agrega logs

---
+++ src/main/java/demo/dogapi/aspect/LogServiceAspect.java
@@ -0,0 +1,44 @@
+package demo.dogapi.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Aspect
+@Component
+public class LogServiceAspect {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LogServiceAspect.class);
+
+    @Around("execution(* demo.dogapi.service.impl..*(..)) && args(request)")
+    public Object log1(ProceedingJoinPoint pjp, String request) throws Throwable {
+        StopWatch sw = new StopWatch();
+        try {
+            sw.start();
+            return pjp.proceed();
+        } finally {
+            sw.stop();
+            LOGGER.info("timelapsed:" + sw.getTotalTimeMillis() + "(ms), " 
+                    + "breed:"+request + ", " 
+                    + " - "+ pjp.getTarget().getClass().getSimpleName() + "." + pjp.getSignature().getName() + "(..)");
+            
+            LOGGER.debug("timelapsed:" + sw.getTotalTimeMillis() + "(ms), " 
+                    + "breed:"+request + ", " 
+                    + " - "+ pjp.getSignature().getName() + "(..))");
+        }
+    }
+    
+    @AfterThrowing(pointcut = "execution(* demo.dogapi.service.impl..*(..))", throwing = "error")
+    public void exceptionLogger(JoinPoint jointPoint, Throwable error) {
+        LOGGER.error(jointPoint.getSignature().getName() + "(..)"
+                    + ", Type:" + error.getClass().getSimpleName()
+                    + ", Message: " + error.getMessage());
+    }
+    
+}

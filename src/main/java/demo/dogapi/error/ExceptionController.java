d1868d286669c868ffcc70634c64af2755bdc190
Elegardo Valdés <elegardo.valdes@equifax.com>
2018-02-27 21:32:33 -0300

    maneja errores por timeout

---
+++ src/main/java/demo/dogapi/error/ExceptionController.java
@@ -0,0 +1,20 @@
+package demo.dogapi.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import demo.dogapi.domain.ResponseError;
+
+@ControllerAdvice
+public class ExceptionController {
+	
+	@ExceptionHandler(ServiceException.class)
+	@ResponseBody
+	public ResponseEntity<ResponseError> handleException(ServiceException e) {
+		return new ResponseEntity<ResponseError>(new ResponseError(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+}
--- src/main/java/demo/dogapi/service/impl/RestServiceImpl.java
+++ src/main/java/demo/dogapi/service/impl/RestServiceImpl.java
@@ -1,15 +1,20 @@
 package demo.dogapi.service.impl;
 
+import java.net.ConnectException;
+import java.net.SocketTimeoutException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 
 import demo.dogapi.domain.APIResult;
 import demo.dogapi.domain.Image;
 import demo.dogapi.domain.Response;
+import demo.dogapi.error.ServiceException;
 import demo.dogapi.service.IRestService;
 
 @Service
@@ -19,14 +24,32 @@ public class RestServiceImpl implements IRestService {
 
     public RestServiceImpl(RestTemplateBuilder restTemplateBuilder) {
         this.restTemplate = restTemplateBuilder.build();
+        setTimeout(restTemplate, 10000);
     }
     
     @Override
 	public Response getDataByBreed(String breed) {
 		
+        Response response = new Response();
+        response.setBreed(breed);
+        
+        try {
+			response.setSubBreeds(getSubBreedList(breed));
+			response.setImages(getImagesList(breed));
+		} catch (ConnectException | ResourceAccessException e) {
+			throw new ServiceException("No se pudo conectar a la API");
+		}
+        
+		return response;
+	}
+    
+    private List<String> getSubBreedList(String breed) throws ConnectException, ResourceAccessException {
 		String breedListURL = String.format("https://dog.ceo/api/breed/%s/list", breed);
 		APIResult breedListResult = restTemplate.getForObject(breedListURL, APIResult.class);
-		
+		return breedListResult.getMessage();    	
+    }
+    
+    private List<Image> getImagesList(String breed) throws ConnectException, ResourceAccessException {
 		String breedImagesURL = String.format("https://dog.ceo/api/breed/%s/images", breed);
 		APIResult breedImagesResult = restTemplate.getForObject(breedImagesURL, APIResult.class);
 		
@@ -35,12 +58,14 @@ public class RestServiceImpl implements IRestService {
         		images.add(new Image(i));
         }
         
-        Response response = new Response();
-        response.setBreed(breed);
-        response.setSubBreeds(breedListResult.getMessage());
-        response.setImages(images);
-        
-		return response;
-	}
+        return images;    	
+    }
+    
+    private void setTimeout(RestTemplate restTemplate, int timeout) {
+        restTemplate.setRequestFactory(new SimpleClientHttpRequestFactory());
+        SimpleClientHttpRequestFactory rf = (SimpleClientHttpRequestFactory) restTemplate.getRequestFactory();
+        rf.setReadTimeout(timeout);
+        rf.setConnectTimeout(timeout);
+    }
 
 }
